f5373e7a69cf3ccba3004f6aec77fe7f4b231752
FINCTIVE <finctive@qq.com>
2021-08-30 17:31:36 +0800

    remove terminal color characters.

--- internal/command.go
+++ internal/command.go
@@ -114,8 +114,6 @@ func CheckBashSyntax(filepath string) error {
 
 const colorANSI = "[\u001B\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))"
 
-var reRemoveColor = regexp.MustCompile(colorANSI)
-
 func StripColorANSI(str string) string {
-	return reRemoveColor.ReplaceAllString(str, "")
+	return regexp.MustCompile(colorANSI).ReplaceAllString(str, "")
 }
